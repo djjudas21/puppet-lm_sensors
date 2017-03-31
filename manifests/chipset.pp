8361ee0552490167c0eb2bcb9edb04f696845f2b
André Bauer <andre.bauer@kiwigrid.com>
2017-03-31 08:52:33 +0200

    fix chipset on vm

--- manifests/chipset.pp
+++ manifests/chipset.pp
@@ -5,28 +5,31 @@ define lm_sensors::chipset (
   String $chip = $title,
 ) {
 
-  if $ensure == 'present' {
-    $real_ensure = 'file'
-  }
-  else {
-    $real_ensure = 'absent'
-  }
+  if $::virtual == 'physical' {
 
-  # create sensors.d dir & chipset file
-  file {
-    $::lm_sensors::sensorsd_dir:
-      ensure  => directory,
-      owner   => 'root',
-      group   => 'root',
-      mode    => '0755',
-      require => Package['lm_sensors'];
-    "${::lm_sensors::sensorsd_dir}/chip_${chip}.conf":
-      ensure  => $real_ensure,
-      owner   => 'root',
-      group   => 'root',
-      mode    => '0644',
-      require => File[$::lm_sensors::sensorsd_dir],
-      notify  => Service['lm_sensors'],
-      content => template('lm_sensors/chipset.conf');
+    if $ensure == 'present' {
+      $real_ensure = 'file'
+    }
+    else {
+      $real_ensure = 'absent'
+    }
+
+    # create sensors.d dir & chipset file
+    file {
+      $::lm_sensors::sensorsd_dir:
+        ensure  => directory,
+        owner   => 'root',
+        group   => 'root',
+        mode    => '0755',
+        require => Package['lm_sensors'];
+      "${::lm_sensors::sensorsd_dir}/chip_${chip}.conf":
+        ensure  => $real_ensure,
+        owner   => 'root',
+        group   => 'root',
+        mode    => '0644',
+        require => File[$::lm_sensors::sensorsd_dir],
+        notify  => Service['lm_sensors'],
+        content => template('lm_sensors/chipset.conf');
+    }
   }
 }
